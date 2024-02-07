cc3944aecfd12bee49018d381da939d19f3e36a7
dotslashf <fadhlurahman.i@gmail.com>
2024-02-08 01:29:34 +0800

    first init

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var firstName string = "fadhlu"
+	var lastName string = "rahman"
+	
+	fmt.Printf("Halo saya %s%s\n", firstName, lastName) // format print
+	fmt.Println("Halo saya", firstName + lastName) // print biasa
+}
