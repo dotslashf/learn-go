7cb306e900c90f37657fb122ba06547174311358
dotslashf <fadhlurahman.i@gmail.com>
2024-02-08 02:11:35 +0800

    feat: data type

--- main.go
+++ main.go
@@ -7,9 +7,18 @@ func main() {
 	var lastName string = "rahman"
 	fullName := firstName + lastName // variable with type inference
 	fullName = "ini budi" // reassign value no need using :
+	var longMessage string = `
+This a message with new line
+Its so long that it used backtick
+	`
 	
 	var one, two, three int = 1, 2, 3
 	four, five, six := 4, 5, 6
+
+	var positiveNumber uint8 = 77
+	negativeNumber := -69
+	var negativeFloatNumber float32 = -69.420
+	var isHuman bool = true
 
 	/*
 	multiple variable with type inference
@@ -20,5 +29,10 @@ func main() {
 	fmt.Printf("Halo saya %s%s\n", firstName, lastName) // format print
 	fmt.Println("Halo saya", firstName + lastName) // normal print
 	fmt.Printf("Nama lengkap saya adalah %s\n", fullName)
-	fmt.Printf("This is number %d, %d, %d, %d, %d, %d", one, two, three, four, five, six)
+	fmt.Printf("This is number %d, %d, %d, %d, %d, %d\n", one, two, three, four, five, six)
+	fmt.Printf("Positive number %d\n", positiveNumber)
+	fmt.Printf("Negative number %d\n", negativeNumber)
+	fmt.Printf("Negative float number %.3f\n", negativeFloatNumber)
+	fmt.Printf("Is %s a human? %t", fullName, isHuman)
+	fmt.Println(longMessage)
 }
