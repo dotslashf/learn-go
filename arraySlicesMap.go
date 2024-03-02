23e378433eb31e5949964c1333a820d8514268d6
fadhlu <38921923+dotslashf@users.noreply.github.com>
2024-03-02 16:03:14 +0800

    feat: slices

--- arraySlicesMap.go
+++ arraySlicesMap.go
@@ -22,9 +22,9 @@ func arraySlicesMap() {
 	}
 	fmt.Println("Fruits:", listOfFruits)
 
-	var numbers = [...]int8{6, 2, 0, 4, 2, 0}
-	fmt.Println("Numbers:", numbers)
-	fmt.Println("Length of numbers:", len(numbers))
+	var numbersInt = [...]int8{6, 2, 0, 4, 2, 0}
+	fmt.Println("Numbers:", numbersInt)
+	fmt.Println("Length of numbers:", len(numbersInt))
 
 	var numbersMultidimensional = [2][5]int8{{4, 1, 3}, {4, 5, 6, 7, 8}}
 	for i := 0; i < len(numbersMultidimensional); i++ {
@@ -48,5 +48,69 @@ func arraySlicesMap() {
 	for _, emoji := range emojis {
 		fmt.Printf("Emoji %s\n", emoji)
 	}
+
+	var languages = []string{"Javascript", "Java", "Go"}
+	fmt.Println(languages[0])
+
+	var languagesSlice = []string{"Javascript", "Java"}  //slice
+	var languagesArray = [2]string{"Go", "Rust"}         //array
+	var languagesArrayDot = [...]string{"Elixir", "C++"} // array
+	fmt.Println(languagesSlice, languagesArray, languagesArrayDot)
+
+	var nationalities = []string{"Indonesian", "Singaporean", "South Korean"}
+	var seaNationalities = nationalities[0:2]
+	fmt.Println(seaNationalities)
+
+	var numbers = []string{"0", "1", "2", "3"}
+	var numbersA = numbers[0:3]
+	var numbersB = numbers[1:4]
+
+	var numbersAA = numbersA[1:2]
+	var numbersAB = numbersB[0:1]
+
+	fmt.Println(numbers, numbersA, numbersB)
+	fmt.Println(numbersAA, numbersAB)
+
+	numbersAB[0] = "69"
+
+	fmt.Println(numbers, numbersA, numbersB)
+	fmt.Println(numbersAA, numbersAB)
+
+	var carBrands = []string{"Honda", "Toyota", "Wuling", "Mitsubishi"}
+	fmt.Println(len(carBrands), cap(carBrands))
+
+	var sliceCarBrands = carBrands[0:3]
+	fmt.Println(len(sliceCarBrands), cap(carBrands))
+
+	var anotherSliceCarBrands = carBrands[1:4]
+	fmt.Println(len(anotherSliceCarBrands), cap(anotherSliceCarBrands))
+
+	var newCarBrands = append(carBrands, "Daihatsu")
+	fmt.Println(newCarBrands)
+
+	var twoCarBrands = carBrands[0:2]
+	fmt.Println(len(twoCarBrands), cap(twoCarBrands))
+	fmt.Println(twoCarBrands, carBrands)
+
+	var addedCarBrands = append(twoCarBrands, "Suzuki")
+	fmt.Println(carBrands, twoCarBrands, addedCarBrands)
+
+	var dstCars = make([]string, 3)
+	var src = carBrands[0:3]
+	var cpyCars = copy(dstCars, src)
+	fmt.Println(dstCars, cpyCars)
+
+	var blankCars = []string{"blank", "blank", "blank"}
+	var srcCars = carBrands[0:2]
+	copy(blankCars, srcCars)
+	fmt.Println(blankCars, srcCars)
+
+	var colors = []string{"Red", "Blue", "Green"}
+	var colorsA = colors[0:2]
+	var colorsB = colors[0:2:2] // index start : index end : cap
+
+	fmt.Println(colors, len(colors), cap(colors))
+	fmt.Println(colorsA, len(colorsA), cap(colorsA))
+	fmt.Println(colorsB, len(colorsB), cap(colorsB))
 
 }
